296adf6c5fc9cb5d7edbc21699daf857a16418b0
Ivan <master.tv@gmail.com>
2016-06-10 00:14:08 +0300

    Initial Commit

---
+++ testRSS/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  testRSS
+//
+//  Created by Ivan on 10.06.16.
+//  Copyright © 2016 i_sokolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
